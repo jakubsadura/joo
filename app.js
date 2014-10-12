2b6bbe1bd75923541dfdb311e3aea9f5c72f9eee
nieswiezy <spam@sadura.pl>
2014-10-12 03:00:37 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var app = http.createServer(function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify('Hello my friend'));
+  console.log('A friend came to see us :)');
+});
+
+app.listen(3000);
