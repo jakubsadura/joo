230f1e83f8d45b7bb53fd77cfa6bd724dcabb422
nieswiezy <spam@sadura.pl>
2014-10-12 03:03:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var app = http.createServer(function (req, res) {
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify('Hello my friend'));
+  res.end(JSON.stringify('Hello my friend, nice to see you here!'));
   console.log('A friend came to see us :)');
 });
 
